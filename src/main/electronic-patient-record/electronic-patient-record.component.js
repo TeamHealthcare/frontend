e7ea8338f936f837b81e2c8297607ff21f260cde
Guillermo <guillermo@Guillermos-iMac.local>
2016-10-08 12:37:57 -0700

    refactored patient list to be its own component. Patient list brings up an add patient modal. This still needs to be hooked up to the API in the future.

--- src/main/electronic-patient-record/electronic-patient-record.component.js
+++ src/main/electronic-patient-record/electronic-patient-record.component.js
@@ -4,40 +4,9 @@
         controller: PatientRecordsController
     });
 
-    PatientRecordsController.$inject = ['$http','DataService', '$uibModal'];
-    function PatientRecordsController($http, DataService, $uibModal) {
+    PatientRecordsController.$inject = [];
+    function PatientRecordsController() {
         var ctrl = this;
-
-        this.patients = [];
-
-        this.openAddPatientModal = openAddPatientModal;
-
-        this.$onInit = getData;
-
-        function getData() {
-            console.log($http);
-            console.log(DataService);
-            DataService.getPatientRecords()
-                .then(function (response) {
-                    console.log('got patient records');
-                    ctrl.patients = response.data
-                })
-        }
-
-    ///--------Patient List Component ------///////
-        function openAddPatientModal() {
-            var modalInstance = $uibModal.open({
-                templateUrl: 'main/electronic-patient-record/add-patient-modal.html',
-                controller: 'addPatientModalController',
-                controllerAs: '$ctrl'
-            });
-
-            modalInstance.result.then(function () {
-                //Send patient to database
-            }, function () {
-                //modal dismissed
-            });
-        }
 
     }
 })();
